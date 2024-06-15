6168a4de68c563b94f11be8914b5b3fa0bcbc0e1
liheng <159103134+lmliheng@users.noreply.github.com>
2024-06-15 19:07:19 +0800

    Delete add.sh

--- add.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-read -p "请输入提交的原因：" REASON
-[ -z "$REASON" ] && { echo "提交原因不能为空"; exit 1; }
-branchName=$(git rev-parse --abbrev-ref HEAD)
-git add .
-git commit -m "$REASON"
-git pull origin "$branchName"
-git push origin "$branchName"
